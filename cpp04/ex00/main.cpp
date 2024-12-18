5f6aa53b9f3e5568750fdb401f61949be2c46bc2
Rihab Raida <rraida-@e1r1p10.1337.ma>
2024-12-19 00:43:49 +0100

    cpp04

---
+++ cpp04/ex00/main.cpp
@@ -0,0 +1,26 @@
+#include "Animal.h" 
+
+
+int main() {
+    Animal a1;
+       a1.makeSound();  
+    // std::cout << "\n=== Testing Animal Class ===" << std::endl;
+    // {
+    //     Animal a1;
+    //     a1.makeSound();  
+    // }
+
+    // std::cout << "\n=== Testing Dog Class ===" << std::endl;
+    // {
+    //     Dog d1;
+    //     d1.makeSound();  
+    // }
+
+    // std::cout << "\n=== Testing Cat Class ===" << std::endl;
+    // {
+    //     Cat c1;
+    //     c1.makeSound(); 
+    // }
+
+    // return 0;
+}
